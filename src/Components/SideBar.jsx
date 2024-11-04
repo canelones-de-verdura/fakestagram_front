ac22c4e0c21368ff8f657b9524aa6b7f2af56e90
Bruno <132522774+TDxBruuno@users.noreply.github.com>
2024-11-04 08:34:58 -0300

    arreglando SideBar

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -1,44 +1,34 @@
-// Sidebar.jsx
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import origin_url from "../Services/Origin";
 import "./Sidebar.css";
 
 const Sidebar = () => {
-  
   const user = JSON.parse(localStorage.getItem("user"));
-    const navigate = useNavigate();
-
-  const handleHome = () => navigate("/feed");
-  const handleNotifications = () => navigate("/notifications");
-  const handleProfile = () => navigate("/profile");
+  const navigate = useNavigate();
 
   return (
     <div className="sidebar">
       <div className="sidebar__logo">Fakestagram</div>
-      <ul className="sidebar__menu">
-        <li className="sidebar__item">
-          <button className="buttonNav" onClick={handleHome}>
-            <span className="material-symbols-outlined">home</span>
-            <span>Home</span>
-          </button>
-        </li>
-        <li className="sidebar__item">
-          <button className="buttonNav" onClick={handleNotifications}>
-            <span className="material-symbols-outlined">notifications</span>
-            <span>Notifications</span>
-          </button>
-        </li>
-        <li className="sidebar__item">
-          <button className="buttonNav" onClick={handleProfile}>
-            <img
-              className="imgNav"
-              src={`${origin_url}/${user.profilePicture}`}
-            />
-            <span>Profile</span>
-          </button>
-        </li>
-      </ul>
+
+      <button className="buttonNavv" onClick={() => navigate("/feed")}>
+        <span className="material-symbols-outlined">home</span>
+        <span>Home</span>
+      </button>
+
+      <button className="buttonNavv" onClick={() => navigate("/notifications")}>
+        <span className="material-symbols-outlined">notifications</span>
+        <span>Notifications</span>
+      </button>
+
+      <button className="buttonNavv" onClick={() => navigate("/profile")}>
+        <img
+          className="imgNav"
+          src={`${origin_url}/${user.profilePicture}`}
+          alt="Profile"
+        />
+        <span>Profile</span>
+      </button>
     </div>
   );
 };
