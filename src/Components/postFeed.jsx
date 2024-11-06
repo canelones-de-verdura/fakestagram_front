61d088c50324a32f228b01ae62a78ac78d3f5249
Bruno <132522774+TDxBruuno@users.noreply.github.com>
2024-11-06 10:25:47 -0300

    arreglando estilos del post

--- src/Components/postFeed.jsx
+++ src/Components/postFeed.jsx
@@ -7,9 +7,6 @@ import "./postFeed.css";
 const Post = ({ post, modalSetOpen, commentsArray }) => {
     const [currentPost, setCurrentPost] = useState(post)
 
-    const likesCount = Array.isArray(post.likes) ? post.likes.length : 0;
-    const handlerLike = () => {
-    }
     const openComments = () => {
         modalSetOpen(true)
         commentsArray({ postID: currentPost._id, comments: currentPost.comments})
@@ -32,17 +29,17 @@ const Post = ({ post, modalSetOpen, commentsArray }) => {
                 <ImageComponent image={`${origin_url}/${currentPost.imageUrl}`} alt_text={currentPost.user.userName} />
             </div>
             <div className="interactionContainer">
-                <div className="likeContainer">
-                    <span className="material-symbols-outlined detailsItems" onClick={handlerLike}>
-                        favorite
-                    </span>
-                    <span className="likesCount">{likesCount}</span>
-                </div>
+                <span className="material-symbols-outlined detailsItems">
+                    favorite
+                </span>
                 <span className="material-symbols-outlined detailsItems" onClick={openComments}>
                     comment
                 </span>
             </div>
             <div className="detailsContainer">
+                <div className="likes">
+                    {currentPost.likes}
+                </div>
                 <div className="descriptionContainer">
                     <p id="nomUsuario">{currentPost.user.username}</p>
                     <p id="description">{currentPost.caption}</p>
