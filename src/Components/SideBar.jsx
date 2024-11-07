fc76477c637a6c9d35a4cf39e3c7dfc3aeeae9a7
Bruno <132522774+TDxBruuno@users.noreply.github.com>
2024-11-07 09:48:15 -0300

    boton logout

--- src/Components/SideBar.jsx
+++ src/Components/SideBar.jsx
@@ -8,6 +8,11 @@ const Sidebar = () => {
   const user = JSON.parse(localStorage.getItem("user"));
   const navigate = useNavigate();
 
+  const handleLogout = () => {
+    localStorage.removeItem("user"); // Eliminar el usuario del localStorage
+    navigate("/login"); // Redirigir al login
+  };
+
   return (
     <div className="sidebar">
       <div className="sidebar__logo">Fakestagram</div>
@@ -28,6 +33,10 @@ const Sidebar = () => {
           username={user.username}
         />
         <span>Profile</span>
+      </button>
+      <button className="buttonNavv logoutButton" onClick={handleLogout}>
+        <span className="material-symbols-outlined">logout</span>
+        <span>Logout</span>
       </button>
     </div>
   );
