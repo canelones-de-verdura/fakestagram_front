a86727ab5e4acc79b62ec182cf93417670156746
Bruno <132522774+TDxBruuno@users.noreply.github.com>
2024-11-07 09:30:34 -0300

    componente perfil por defecto

--- src/Components/postFeed.jsx
+++ src/Components/postFeed.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import ImageComponent from "./ImageComponent";
 import origin_url from "../Services/Origin";
+import ProfilePhoto from "./ProfilePhoto";
 
 import "./postFeed.css";
 
@@ -21,46 +22,55 @@ const Post = ({ post, modalSetOpen, commentsArray }) => {
     }
 
     return (
-        <>
-            <div className="userInfo">
-                <div className="nomImgContainer">
-                    <img src={`${origin_url}/${currentPost.user.profilePicture}`} alt={`${currentPost.user.username} avatar`} />
-                    <p id="nomUsuario">{currentPost.user.username}</p>
-                </div>
-                <button id="optionsButton">
-                    <span className="material-symbols-outlined">
-                        more_vert
-                    </span>
-                </button>
-            </div>
-            <div className="imgContainer">
-                <ImageComponent image={`${origin_url}/${currentPost.imageUrl}`} alt_text={currentPost.user.userName} />
-            </div>
-            <div className="interactionContainer">
+      <>
+        <div className="userInfo">
+          <div className="nomImgContainer">
+            <ProfilePhoto
+              profilePicture={currentPost.user.profilePicture}
+              username={currentPost.user.username}
+            />
+            <p id="nomUsuario">{currentPost.user.username}</p>
+          </div>
+          <button id="optionsButton">
+            <span className="material-symbols-outlined">more_vert</span>
+          </button>
+        </div>
+        <div className="imgContainer">
+          <ImageComponent
+            image={`${origin_url}/${currentPost.imageUrl}`}
+            alt_text={currentPost.user.userName}
+          />
+        </div>
+        <div className="interactionContainer">
+          <div className="likeContainer">
+            <span
+              className={`material-symbols-outlined detailsItems ${
+                liked ? "liked" : ""
+              }`}
+              onClick={handlerLike}
+            >
+              {" "}
+              favorite
+            </span>
+            <span className="likesCount">{likesCount}</span>
+          </div>
 
-                <div className="likeContainer">
-                    <span className={`material-symbols-outlined detailsItems ${liked ? "liked" : ""}`} 
-                        onClick={handlerLike}
-                    >    favorite
-                    </span>
-                    <span className="likesCount">{likesCount}</span>
-                </div>
-
-                <span className="material-symbols-outlined detailsItems" onClick={openComments}>
-                    comment
-                </span>
-            </div>
-            <div className="detailsContainer">
-                <div className="likes">
-                    {currentPost.likes}
-                </div>
-                <div className="descriptionContainer">
-                    <p id="nomUsuario">{currentPost.user.username}</p>
-                    <p id="description">{currentPost.caption}</p>
-                </div>
-            </div>
-        </>
-    )
+          <span
+            className="material-symbols-outlined detailsItems"
+            onClick={openComments}
+          >
+            comment
+          </span>
+        </div>
+        <div className="detailsContainer">
+          <div className="likes">{currentPost.likes}</div>
+          <div className="descriptionContainer">
+            <p id="nomUsuario">{currentPost.user.username}</p>
+            <p id="description">{currentPost.caption}</p>
+          </div>
+        </div>
+      </>
+    );
 }
 
 export default Post;
